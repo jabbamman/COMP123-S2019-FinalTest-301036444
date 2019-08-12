a5529877820609b55282082c12de39379a2d292f
MOHAMMED ALI <PT.JABBA@outlook.com>
2019-08-12 12:53:13 -0400

    Added Character & Item Classes

---
+++ COMP123-S2019-FinalTest-301036444/Objects/Character.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Student Name: Mohamed A. Ali 
+ * Student ID: 301036444
+ * Description: this is the character calss used in character creationb this 
+ * is also the data container for applicattion
+ * 
+ */
+
+
+namespace COMP123_S2019_FinalTest_301036444.Objects
+{
+    class Character
+    {
+        // 
+        public string Strength { get; set; }
+        public string Desxterity { get; set; }
+        public string Constitution { get; set; }
+        public string Intelligence { get; set; }
+        public string Wisdom { get; set; }
+        public string Charisma { get; set; }
+
+        // Secondary Ability
+        public int ArmourClass { get; set; }
+        public int HitPoints { get; set; }
+
+        // Chararcter Class
+        public string  CharacterClass { get; set; }
+        public int Level { get; set; }
+
+        // Equipment
+        List<Item> Inventory;
+
+        // Constructor 
+        Character()
+        {
+            this.Inventory = new List<Item>();
+        }
+    }
+}
---
+++ COMP123-S2019-FinalTest-301036444/Objects/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Student Name: Mohamed A. Ali 
+ * Student ID: 301036444
+ * Description: this is the character calss used in character creationb this 
+ * is also the data container for applicattion
+ * 
+ */
+
+
+namespace COMP123_S2019_FinalTest_301036444.Objects
+{
+    class Item
+    {
+        public string Description { get; set; }
+        public float Weight { get; set; }
+        public int Cost { get; set; }
+    }
+}
